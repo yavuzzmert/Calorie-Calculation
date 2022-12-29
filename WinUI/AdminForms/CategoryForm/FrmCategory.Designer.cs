d40eb0aef7f2685ef36f3358964f01caf0fc50e0
Yavuz <yavuz.mert@outlook.com>
2022-12-29 22:36:04 +0300

    AdmiForm içerisindeki categoryForm oluşturuldu ve gerekli kodlamaları yazıldı.

--- WinUI/AdminForms/CategoryForm/FrmCategory.Designer.cs
+++ WinUI/AdminForms/CategoryForm/FrmCategory.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.lstCategory = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Kategori Adı :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Kategori Listesi :";
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Location = new System.Drawing.Point(439, 69);
+            this.txtCategoryName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(418, 27);
+            this.txtCategoryName.TabIndex = 8;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(690, 161);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(167, 53);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Tag = "Delete";
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(516, 161);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(167, 53);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Tag = "Update";
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(343, 161);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(167, 53);
+            this.btnNew.TabIndex = 7;
+            this.btnNew.Tag = "Create";
+            this.btnNew.Text = "Yeni Oluştur";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.Click);
+            // 
+            // lstCategory
+            // 
+            this.lstCategory.FormattingEnabled = true;
+            this.lstCategory.ItemHeight = 20;
+            this.lstCategory.Location = new System.Drawing.Point(23, 65);
+            this.lstCategory.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lstCategory.Name = "lstCategory";
+            this.lstCategory.Size = new System.Drawing.Size(250, 384);
+            this.lstCategory.TabIndex = 4;
+            // 
+            // FrmCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(887, 473);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCategoryName);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.lstCategory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmCategory";
             this.Text = "FrmCategory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label3;
+        private Label label1;
+        private TextBox txtCategoryName;
+        private Button btnDelete;
+        private Button btnUpdate;
+        private Button btnNew;
+        private ListBox lstCategory;
     }
 }
--- WinUI/AdminForms/CategoryForm/FrmCategory.cs
+++ WinUI/AdminForms/CategoryForm/FrmCategory.cs
@@ -1,20 +1,154 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using BLL.Abstract;
+using Entities;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using Systm;
+using ViewModels.CategoryViewModels;
 
 namespace WinUI.AdminForms.CategoryForm
 {
     public partial class FrmCategory : Form
     {
-        public FrmCategory()
+        private readonly ICategoryBLL _categoryBLL;
+
+        public FrmCategory(ICategoryBLL categoryBLL)
         {
+            _categoryBLL = categoryBLL;
             InitializeComponent();
+        }
+
+        private void frmCategory_Load(object sender, EventArgs e)
+        {
+            FillWithList();
+        }
+
+        private void FillWithList()
+        {
+             lstCategory.Items.Clear();
+            ResultService<Category> result = _categoryBLL.GetAllCategories();
+
+            if (!result.HasError)
+            {
+                string[] categoryNames = result.ListData.Select(x => x.CategoryName).ToArray();
+
+                lstCategory.Items.AddRange(categoryNames);
+            }
+        }
+
+        private void SelectedIndex(object sender, EventArgs e)
+        {
+            string categoryName = lstCategory.SelectedItem.ToString();
+            txtCategoryName.Text = _categoryBLL.GetCategoryId(categoryName).Data.Name;
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            string tag = btn.Tag.ToString();
+
+            switch(tag)
+            {
+                case "Create": 
+                        CreateCategory();
+                    break;
+                case "Update":
+                        UpdateCategory();
+                    break;
+                case "Delete":
+                        DeleteCategory();
+                    break;
+            }
+        }
+
+        private void DeleteCategory()
+        {
+            string categoryName = lstCategory.SelectedItem as string;
+            ResultService<Category> result = _categoryBLL.GetCategoryByName(categoryName);
+
+            if (!result.HasError)
+            {
+                CategoryBaseVM @base = new CategoryBaseVM()
+                {
+                    Id = result.Data.ID,
+                };
+                ResultService<CategoryBaseVM> baseResult = _categoryBLL.DeleteCategoryById(@base.Id);
+                if (baseResult.HasError)
+                {
+                    string errorMessage = baseResult.ErrorItems.First().ErrorMessage;
+                    string errorType = baseResult.ErrorItems.First().ErrorType;
+                    MessageBox.Show(errorMessage, errorType, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Silme işlemi başarılı!", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    FillWithList();
+                }
+            }
+            else
+            {
+                string errorMessage = result.ErrorItems.First().ErrorMessage;
+                string errorType = result.ErrorItems.First().ErrorType;
+                MessageBox.Show(errorMessage, errorType, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateCategory()
+        {
+            string categoryName = lstCategory.SelectedItem as string;
+            ResultService<Category> result = _categoryBLL.GetCategoryByName(categoryName);
+
+            if (!result.HasError)
+            {
+                CategoryUpdateVM vm = new CategoryUpdateVM()
+                {
+                    Name = txtCategoryName.Text,
+                    Id = result.Data.ID,
+                };
+                ResultService<CategoryUpdateVM> updateResult = _categoryBLL.UpdateCategory(vm);
+                if (updateResult.HasError)
+                {
+                    string errorMessage = updateResult.ErrorItems.First().ErrorMessage;
+                    string errorType = updateResult.ErrorItems.First().ErrorType;
+                    MessageBox.Show(errorMessage, errorType, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Güncelleme işlemi Başarılı", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    FillWithList();
+                }
+            }
+            else
+            {
+                string errorMessage = result.ErrorItems.First().ErrorMessage;
+                string errorType = result.ErrorItems.First().ErrorType;
+                MessageBox.Show(errorMessage, errorType, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CreateCategory()
+        {
+            CategoryCreateVM vm = new CategoryCreateVM()
+            {
+                CategoryName = txtCategoryName.Text,
+            };
+
+            ResultService<Category> result = _categoryBLL.CreateCategory(vm);
+            GetResult(result);
+            FillWithList();
+        }
+
+        private void GetResult(ResultService<Category> result)
+        {
+            if (result.HasError)
+            {
+                string errorMessage = result.ErrorItems.FirstOrDefault().ErrorMessage;
+                string errorType = result.ErrorItems.FirstOrDefault().ErrorType;
+
+                MessageBox.Show(errorMessage,errorType, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Kayıt işlemi başarılı!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
