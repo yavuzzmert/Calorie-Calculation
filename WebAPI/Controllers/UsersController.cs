7a3daec015d4b1c1344e8398eaea973986adca3f
Yavuz <yavuz.mert@outlook.com>
2023-01-02 23:29:31 +0300

    user için WebAPI oluşturuldu

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,105 @@
+﻿using DAL.Concrete.Context;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DietDBContext _dietDBContext;
+
+        public UsersController(DietDBContext dietDBContext)
+        {
+            _dietDBContext = dietDBContext;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _dietDBContext.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _dietDBContext.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.ID)
+            {
+                return BadRequest();
+            }
+
+            _dietDBContext.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _dietDBContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _dietDBContext.Users.Add(user);
+            await _dietDBContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.ID }, user);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _dietDBContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _dietDBContext.Users.Remove(user);
+            await _dietDBContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _dietDBContext.Users.Any(e => e.ID == id);
+        }
+    }
+}
